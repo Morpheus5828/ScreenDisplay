5a7885075ce45c6193270b9dcff29d90015efdb6
Marius THORRE Portable <marius.thorre@etu.univ-amu.fr>
2023-01-17 18:55:22 +0100

    Add FXML basical pages (and event in controller)

---
+++ src/main/java/com/screendisplay/Controller/Controller.java
@@ -0,0 +1,13 @@
+package com.screendisplay.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML public Button screen;
+    @FXML public Button playlist;
+    @FXML public Button document;
+    @FXML public Button user;
+
+}
