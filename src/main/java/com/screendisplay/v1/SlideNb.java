c5e908eb401dbb045ed5e1fa36899594cc05e1b6
Marius THORRE Portable <marius.thorre@etu.univ-amu.fr>
2023-02-27 14:21:59 +0100

    Add Slide 0-1-2 and actionEvent to switch into slides

--- src/main/java/com/screendisplay/v1/SlideNb.java
+++ src/main/java/com/screendisplay/v1/SlideNb.java
@@ -1,8 +1,9 @@
 package com.screendisplay.v1;
 
+import com.screendisplay.v2.Controller.Slide.Slide;
+
 public enum SlideNb {
     SLIDE0, SLIDE1, SLIDE2;
-
 
     public SlideNb nextSlide(SlideNb slideNb) {
         switch (slideNb) {
@@ -18,5 +19,39 @@ public enum SlideNb {
                 return SLIDE0;
             }
         }
+        return slideNb;
     }
+
+    public SlideNb beforeSlide(SlideNb slideNb) {
+        switch (slideNb) {
+            case SLIDE0 -> {
+                return SLIDE2;
+            }
+
+            case SLIDE1 -> {
+                return SLIDE0;
+            }
+
+            case SLIDE2 -> {
+                return SLIDE1;
+            }
+        }
+        return slideNb;
+    }
+
+    public String getPath() {
+        switch (this) {
+            case SLIDE0 -> {
+                return "C:\\Users\\thorr\\IdeaProjects\\ScreenDisplay\\src\\main\\resources\\com\\screendisplay\\version 1.0\\slide0.fxml";
+            }
+            case SLIDE1 -> {
+                return "C:\\Users\\thorr\\IdeaProjects\\ScreenDisplay\\src\\main\\resources\\com\\screendisplay\\version 1.0\\slide1.fxml";
+            }
+            case SLIDE2 -> {
+                return "C:\\Users\\thorr\\IdeaProjects\\ScreenDisplay\\src\\main\\resources\\com\\screendisplay\\version 1.0\\slide2.fxml";
+            }
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/screendisplay/v1/V1Controller/V1Controller.java
+++ src/main/java/com/screendisplay/v1/V1Controller/V1Controller.java
@@ -1,14 +1,52 @@
 package com.screendisplay.v1.V1Controller;
 
+import com.screendisplay.v1.SlideNb;
+import com.screendisplay.v2.Controller.Slide.Slide;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.Buffer;
 
 public class V1Controller {
+    private SlideNb currentSlide = SlideNb.SLIDE0;
+    @FXML private StackPane v1Stackpane;
     @FXML private Button left_btn;
     @FXML private Button right_btn;
 
 
+    public void changeToSeeRightSlide(ActionEvent actionEvent) throws IOException {
+        setCurrentSlide(currentSlide.nextSlide(currentSlide));
+        String path = currentSlide.getPath();
+        System.out.println(path);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(new File(path).toURI().toURL());
+        Parent root = fxmlLoader.load();
+        v1Stackpane.getChildren().setAll(root);
+        v1Stackpane.getChildren().removeAll();
+        v1Stackpane.getChildren().setAll(root);
+        System.out.println(currentSlide);
+    }
 
+    public void changeToSeeLeftSlide(ActionEvent actionEvent) throws IOException {
+        setCurrentSlide(currentSlide.beforeSlide(currentSlide));
+        String path = currentSlide.getPath();
+        System.out.println(path);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(new File(path).toURI().toURL());
+        Parent root = fxmlLoader.load();
+        v1Stackpane.getChildren().setAll(root);
+        v1Stackpane.getChildren().removeAll();
+        v1Stackpane.getChildren().setAll(root);
+        System.out.println(currentSlide);
+    }
+
+    public void setCurrentSlide(SlideNb currentSlide) {
+        this.currentSlide = currentSlide;
+    }
 }
